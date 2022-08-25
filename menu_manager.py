85d53ec4a3ff5a53a2e94b0a417018a30118210a
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2022-08-25 08:45:34 +0200

    Add list view to menu

--- menu_manager.py
+++ menu_manager.py
@@ -50,30 +50,34 @@ class CRUDMenu(BaseMenu):
     def __init__(self, table: ObjectTable):
         super().__init__()
         self.table = table
+        self.add_choice(MenuChoice("List", self.list))
         self.add_choice(MenuChoice("Create", self.create))
         self.add_choice(MenuChoice("Update", self.update))
         self.add_choice(MenuChoice("Delete", self.delete))
 
+    def list(self) -> None:
+        print(self.table.format_list())
+
     def prompt(self):
         print("######### " + self.table.object_class.__class__.__name__ + " ###########")
         return super().prompt()
-    
+
     def create(self):
         newdata = {}
         for attr, attrname in self.table.object_class.prompt_attrs.items():
             newdata[attr] = input(f"Insert {attrname}: ")
-            
+
         new_obj = self.table.object_class(**newdata)
         self.table.add(new_obj)
-        
+
     def update(self):
         obj = self.table.interactive_get()
-        
+
         for attr, attrname in self.table.object_class.prompt_attrs.items():
             newval = input(f"Insert {attrname} [{getattr(obj, attr)}]: ")
             if newval != "":
                 setattr(obj, attr, newval)
-            
+
     def delete(self):
         obj = self.table.interactive_get()
         self.table.remove(obj)
--- palestra.py
+++ palestra.py
@@ -1,19 +1,20 @@
 from objects import TrainingClass, Member, Trainer, ObjectTable
 from menu_manager import BaseMenu, CRUDMenu, MenuChoice
 
+
 def main(members_list, trainers_list, training_class_list):
     members_menu = CRUDMenu(members_list)
     trainers_menu = CRUDMenu(trainers_list)
     training_class_menu = CRUDMenu(training_class_list)
-    
-    
+
     base = BaseMenu()
     base.add_choice(MenuChoice("Manage Members", members_menu.prompt))
     base.add_choice(MenuChoice("Manage Trainers", trainers_menu.prompt))
-    base.add_choice(MenuChoice("Manage Training Classes", training_class_menu.prompt))
-    
+    base.add_choice(MenuChoice("Manage Training Classes",
+                    training_class_menu.prompt))
+
     base.prompt()
-    
+
 
 if __name__ == "__main__":
     members_list = ObjectTable(Member)
